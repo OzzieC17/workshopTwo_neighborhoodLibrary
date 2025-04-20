36f64ce4e29bd1df4126eba3253b62266a464951
ozzie <ocarreon@appdev.yearup.org>
2025-04-20 18:36:22 -0400

    books java class finished and menu promt created

--- src/main/java/com/ps/Books.java
+++ src/main/java/com/ps/Books.java
@@ -1,5 +1,76 @@
 package com.ps;
 
 public class Books {
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
 
+    public Books(String checkedOutTo, boolean isCheckedOut, String title, String isbn, int id) {
+        this.checkedOutTo = checkedOutTo;
+        this.isCheckedOut = isCheckedOut;
+        this.title = title;
+        this.isbn = isbn;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    public void checkOut(String name) {
+        if (!isCheckedOut) {
+            isCheckedOut = true;
+            checkedOutTo = name;
+            System.out.println(title + " has been checked out to " + name + ".");
+        } else {
+            System.out.println(title + " is already checked out.");
+        }
+    }
+    public void checkIn(){
+            if(isCheckedOut){
+                isCheckedOut = false;
+                checkedOutTo = "";
+                System.out.println(title + " has been returned ");
+            }else {
+                System.out.println(title + " is not currently checked out.");
+            }
+    }
 }
--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,20 @@
 package com.ps;
 
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Neighborhood Library");
+        System.out.println("1) Show available books :");
+        System.out.println("2) Show checked out books:");
+        System.out.println("3) Exit");
+
+        String givenCommand = scanner.nextLine();
+
+
 
     }
 }
