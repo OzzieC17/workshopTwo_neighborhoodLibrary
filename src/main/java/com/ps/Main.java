b2b99258763b54bc35d1774795da93a1d34ac3d2
ozzie <ocarreon@appdev.yearup.org>
2025-04-20 21:02:27 -0400

    book availability, new changes to display book genre

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -7,14 +7,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Books[] inventory = new Books[20];
+        String[] bookGenres = {"Mystery", "Fantasy", "Sci-Fi", "Romance", "Thriller"};
+        for (int i = 0; i < inventory.length; i++) {
+            String title = bookGenres[i % bookGenres.length] + " Book " + (i + 1);
+            inventory[i] = new Books("", false, title, "ISBN" + (1000 + i), i + 1);
+        }
+
+
         System.out.println("Neighborhood Library");
-        System.out.println("1) Show available books :");
-        System.out.println("2) Show checked out books:");
-        System.out.println("3) Exit");
+        System.out.println("1) Show available books");
+        System.out.println("2) Show checked out books");
+        System.out.println("3) Check out");
+        System.out.println("4) Check in");
 
         String givenCommand = scanner.nextLine();
 
+        switch (givenCommand) {
+            case "1":
+                System.out.println("\nAvailable Books:");
+                for (Books b : inventory) {
+                    if (!b.isCheckedOut()) {
+                        System.out.println("ID: " + b.getId() + " | Title: " + b.getTitle());
+                    }
+                }
+                break;
 
-
+            case "2":
+                System.out.println("\nChecked Out Books:");
+                for (Books b : inventory) {
+                    if (b.isCheckedOut()) {
+                        System.out.println("ID: " + b.getId() + " | Title: " + b.getTitle() +
+                                " | Checked out to: " + b.getCheckedOutTo());
+                    }
+                }
+                break;
+        }
     }
 }
